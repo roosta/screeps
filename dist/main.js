78393fd78c2caaeb0cd1eac80a5b6888320b37df
Daniel Berg <mail@roosta.sh>
2016-07-17 20:48:57 +0200

    Add for-in error checking. Fix path in require.

--- dist/main.js
+++ dist/main.js
@@ -1,23 +1,29 @@
-const roleHarvester = require('role.harvester');
-const roleUpgrader = require('role.upgrader');
-const roleBuilder = require('role.builder');
-const handlerSpawner = require('handler.spawner');
+const roleHarvester  = require('./role.harvester');
+const roleUpgrader   = require('./role.upgrader');
+const roleBuilder    = require('./role.builder');
+const handlerSpawner = require('./handler.spawner');
 
 module.exports.loop = function main() {
   console.log('-----TICK-----');
   handlerSpawner.garbageCollect();
   handlerSpawner.run();
 
-  for(var name in Game.creeps) {
-    var creep = Game.creeps[name];
-    if(creep.memory.role == 'harvester') {
-      roleHarvester.run(creep);
-    }
-    if(creep.memory.role == 'upgrader') {
-      roleUpgrader.run(creep);
-    }
-    if(creep.memory.role == 'builder') {
-      roleBuilder.run(creep);
+  for (let name in Game.creeps) {
+    if ({}.hasOwnProperty.call(Game.creeps, name)) {
+      let creep = Game.creeps[name];
+      switch (creep.memory.role) {
+        case 'harvester':
+          roleHarvester.run(creep);
+          break;
+        case 'upgrader':
+          roleUpgrader.run(creep);
+          break;
+        case 'builder':
+          roleBuilder.run(creep);
+          break;
+        default:
+          console.error(`fell through @ main. Include: ${creep.memory.role}?`);
+      }
     }
   }
-}
+};
