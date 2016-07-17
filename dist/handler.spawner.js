cefd12e6a2a1337f294eac351d20ed3474eabc12
Daniel Berg <mail@roosta.sh>
2016-07-17 19:06:47 +0200

    Fix wrong constant ref.

--- dist/handler.spawner.js
+++ dist/handler.spawner.js
@@ -7,39 +7,38 @@
  * mod.thing == 'a thing'; // true
  */
 
-/* global Game */
-const _ = require('lodash');
-
 module.exports = {
   run: function run() {
-    const harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === 'harvester');
-    const upgraders = _.filter(Game.creeps, (creep) => creep.memory.role === 'upgrader');
-    const builders = _.filter(Game.creeps, (creep) => creep.memory.role === 'builder');
+    let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === 'harvester');
+    let upgraders = _.filter(Game.creeps, (creep) => creep.memory.role === 'upgrader');
+    let builders = _.filter(Game.creeps, (creep) => creep.memory.role === 'builder');
     console.log(`Harvesters: ${harvesters.length}`);
     console.log(`Upgraders: ${upgraders.length}`);
     console.log(`Builders: ${builders.length}`);
 
     if (harvesters.length < 2) {
-      const newName = Game.spawns.Spawn1.createCreep(
-        [Game.WORK, Game.CARRY, Game.MOVE],
+      let newName = Game.spawns.Spawn1.createCreep(
+        [WORK, CARRY, MOVE],
         undefined,
         { role: 'harvester' }
       );
       if (newName > -1) {
         console.log(`Spawning new harvester: ${newName}`);
       }
-    } else if (upgraders.length < 2) {
-      const newName = Game.spawns.Spawn1.createCreep(
-        [Game.WORK, Game.CARRY, Game.MOVE],
+    }
+    if (upgraders.length < 2) {
+      let newName = Game.spawns.Spawn1.createCreep(
+        [WORK, CARRY, MOVE],
         undefined,
         { role: 'upgrader' }
       );
       if (newName > -1) {
         console.log(`Spawning new upgrader: ${newName}`);
       }
-    } else if (builders.length < 2) {
-      const newName = Game.spawns.Spawn1.createCreep(
-        [Game.WORK, Game.CARRY, Game.MOVE],
+    }
+    if (builders.length < 2) {
+      let newName = Game.spawns.Spawn1.createCreep(
+        [WORK, CARRY, MOVE],
         undefined,
         { role: 'builder' }
       );
@@ -50,8 +49,8 @@ module.exports = {
   },
   // Always place this memory cleaning code at the very top of your main loop!
   garbageCollect: function garbageCollect() {
-    for (const name in Memory.creeps) {
-      if(!Game.creeps[name]) {
+    for (let name in Memory.creeps) {
+      if (!Game.creeps[name]) {
         delete Memory.creeps[name];
         console.log('Clearing non-existing creep memory:', name);
       }
