061314d1b4bdf6372c1ebe2b83916f1bf19a6dc3
Daniel Berg <mail@roosta.sh>
2016-07-17 16:32:13 +0200

    Finish linting spawner.

--- src/handler.spawner.js
+++ src/handler.spawner.js
@@ -7,21 +7,49 @@
  * mod.thing == 'a thing'; // true
  */
 
+/* global Game */
+const _ = require('lodash');
+
 module.exports = {
-    run: function() {
-        var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-        console.log('Harvesters: ' + harvesters.length);
-    
-        if(harvesters.length < 2) {
-            var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
-            console.log('Spawning new harvester: ' + newName);
-        }
-        
-        var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-        console.log('Upgrader: ' + harvesters.length);
-        if(upgraders.length < 2) {
-            var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
-            console.log('Spawning new upgrader: ' + newName);
-        }
+  run: function run() {
+    const harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === 'harvester');
+    const upgraders = _.filter(Game.creeps, (creep) => creep.memory.role === 'upgrader');
+    const builders = _.filter(Game.creeps, (creep) => creep.memory.role === 'builder');
+    console.log(`Harvesters: ${harvesters.length}`);
+    console.log(`Upgraders: ${upgraders.length}`);
+    console.log(`Builders: ${builders.length}`);
+
+    if (harvesters.length < 2) {
+      const newName = Game.spawns.Spawn1.createCreep(
+        [Game.WORK, Game.CARRY, Game.MOVE],
+        undefined,
+        { role: 'harvester' }
+      );
+      if (newName > -1) {
+        console.log(`Spawning new harvester: ${newName}`);
+      }
     }
+
+    if (upgraders.length < 2) {
+      const newName = Game.spawns.Spawn1.createCreep(
+        [Game.WORK, Game.CARRY, Game.MOVE],
+        undefined,
+        { role: 'upgrader' }
+      );
+      if (newName > -1) {
+        console.log(`Spawning new upgrader: ${newName}`);
+      }
+    }
+
+    if (builders.length < 2) {
+      const newName = Game.spawns.Spawn1.createCreep(
+        [Game.WORK, Game.CARRY, Game.MOVE],
+        undefined,
+        { role: 'builder' }
+      );
+      if (newName > -1) {
+        console.log(`Spawning new builder: ${newName}`);
+      }
+    }
+  },
 };
