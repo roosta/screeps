f2b97304267d2a0643358f50d51aa516dae5047e
Daniel Berg <mail@roosta.sh>
2016-07-18 22:35:29 +0200

    Add prototype role, repair: common function.

---
+++ dist/role.prototype.js
@@ -0,0 +1,26 @@
+let proto = {
+  repair: function repair(creep) {
+    let closestDamagedStructure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+      filter: (structure) => structure.hits < structure.hitsMax,
+    });
+    if (creep.memory.repairing && creep.carry.energy === 0) {
+      creep.memory.repairing = false;
+    }
+    if (!creep.memory.repairing &&
+        creep.carry.energy === creep.carryCapacity &&
+        closestDamagedStructure) {
+      creep.memory.repairing = true;
+    }
+
+    if (creep.memory.repairing) {
+      if (creep.repair(closestDamagedStructure) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(closestDamagedStructure);
+      } else {
+        creep.repair(closestDamagedStructure);
+      }
+    }
+  },
+};
+
+module.exports = proto;
+
